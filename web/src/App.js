077cd80e7f280f8b004683932dc7181e8bccb7fe
matart15 <matart15@gmail.com>
2018-03-14 13:36:33 +0900

    simple web graphql

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,73 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+  NavLink,
+  Link,
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
-  }
-}
+import FeedPage from './containers/FeedPage'
+import DraftsPage from './containers/DraftsPage'
+import CreatePage from './containers/CreatePage'
+import DetailPage from './containers/DetailPage'
 
-export default App
+import './index.css'
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+export default () => (
+  <ApolloProvider client={client}>
+    <Router>
+      <React.Fragment>
+        <nav className="pa3 pa4-ns">
+          <Link
+            className="link dim black b f6 f5-ns dib mr3"
+            to="/"
+            title="Feed">
+            Blog
+          </Link>
+          <NavLink
+            className="link dim f6 f5-ns dib mr3 black"
+            activeClassName="gray"
+            exact
+            to="/"
+            title="Feed">
+            Feed
+          </NavLink>
+          <NavLink
+            className="link dim f6 f5-ns dib mr3 black"
+            activeClassName="gray"
+            exact
+            to="/drafts"
+            title="Drafts">
+            Drafts
+          </NavLink>
+          <Link
+            to="/create"
+            className="f6 link dim br1 ba ph3 pv2 fr mb2 dib black">
+            + Create Draft
+          </Link>
+        </nav>
+        <div className="fl w-100 pl4 pr4">
+          <Switch>
+            <Route exact path="/" component={FeedPage} />
+            <Route path="/drafts" component={DraftsPage} />
+            <Route path="/create" component={CreatePage} />
+            <Route path="/post/:id" component={DetailPage} />
+          </Switch>
+        </div>
+      </React.Fragment>
+    </Router>
+  </ApolloProvider>
+)
